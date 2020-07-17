dc0ce6faf193597fbd4a0923f4f549797861399c
in-in <dmin@tuta.io>
2020-07-17 04:16:58 +0300

    feat(favicon): add favicons

--- .config/webpack.config.js
+++ .config/webpack.config.js
@@ -1,6 +1,7 @@
 const path = require('path');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 const HtmlWebpackInlineSVGPlugin = require('html-webpack-inline-svg-plugin');
+const FaviconsWebpackPlugin = require('favicons-webpack-plugin');
 const classnames = require('classnames');
 const { PATHS, PAGES } = require('./paths');
 
@@ -78,6 +79,22 @@ const config = {
 		),
 		new HtmlWebpackInlineSVGPlugin({
 			'runPreEmit': true,
+		}),
+		new FaviconsWebpackPlugin({
+			'logo': path.join(PATHS.src, 'assets', 'favicon.svg'),
+			'outputPath': path.join(PATHS.assets, 'favicon'),
+			'prefix': path.join(PATHS.assets, 'favicon'),
+			'inject': true,
+			'favicons': {
+				'appName': 'Lithium',
+				'icons': {
+					'appleStartup': false,
+					'coast': false,
+					'firefox': false,
+					'windows': false,
+					'yandex': false,
+				},
+			},
 		}),
 	],
 };
